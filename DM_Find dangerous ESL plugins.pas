8db65e37cc8d3b2a4416a85d19a500c5ee6a4cc5
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2022-06-09 17:47:20 -0500

    Update DM_Find dangerous ESL plugins.pas
    Now it uses the sturdier "Check" function from xEdit.

--- DM_Find dangerous ESL plugins.pas
+++ DM_Find dangerous ESL plugins.pas
@@ -5,20 +5,23 @@ unit FindDangerousESL;
 
 const
   iESLMaxRecords = $800;  // max possible new records in ESL
-  iESLMaxFormID = $fff;   // max allowed FormID number in ESL
 
   AnalysisSuccess = 0;
   HasTooManyRecords = 2;
-  HasIdOverflow = 3;
+  HasOtherError = 3;
 
 var 
-  HasErrors: Boolean;
+  errors: TStringList;
 
 function CheckRecords(f: IInterface): Integer;
 var
   i: Integer;
   e: IInterface;
   RecCount, RecMaxFormID, fid: Cardinal;
+  msg: string;
+const 
+  overflowFmt = '%s > "%s": %s';
+  manyRecsFmt = 'Plugin has too many records and can not ever be turned into an ESL.';
 begin
   Result := AnalysisSuccess;
 
@@ -30,43 +33,65 @@ begin
     
     Inc(RecCount);    
     if RecCount > iESLMaxRecords then begin
+      errors.Add(manyRecsFmt);
       Result := HasTooManyRecords;
       Exit;
     end;
     
-    fid := FormID(e) and $FFF;
-    if fid > iESLMaxFormID then begin
-      AddMessage(IntToHex(fid, 4));
-      Result := HasIdOverflow;
-      Exit;
-    end;
+    msg := Check(e);
+    if msg <> '' then begin
+      errors.Add(Format(overflowFmt, [Signature(e), EditorID(e), msg]));
+      Result := HasOtherError;
+    end;     
   end;
 end;
 
 procedure DisplayAnalysisMessage(espName: string; RecordAnalysis: Integer);
 var
   msg: string;
+  i: Integer;
+  ctdFriendly: Boolean;
 const 
-  corruption = ' If you continue to use this plugin, it will surely lead to CTDs.';
+  corruption = #13#10'If you continue to use this plugin, it will surely lead to CTDs.';
+  successFmt = '%s has no errors.';
+  errorsFmt = '%s has errors:';
+  errorFmt = '    %s';
+  sep = '**********************************************';
 begin
-  HasErrors := (RecordAnalysis <> AnalysisSuccess) or HasErrors;
-
-  case RecordAnalysis of
-    HasTooManyRecords:
-      msg := '***ERROR***: Plugin has too many records and can''t ever be turned into an ESL.' + corruption;
-    HasIdOverflow:
-      msg := '***ERROR***: Plugin has a record with a FormID out of bounds. Contact the author so they can remove the ESL flag, compact FormIDs and then turn on the ESL flag again.' + corruption;
-    else 
-      msg := '';
+  if RecordAnalysis = AnalysisSuccess then begin
+    AddMessage(Format(successFmt, [espName]));
+    Exit;
   end;
-  
-  if msg = '' then Exit;
-  AddMessage(espName);
-  AddMessage(#9 + msg);
+
+  ctdFriendly := false;
+
+  AddMessage(sep);
+  AddMessage(Format(errorsFmt, [espName]));
+
+  for i := 0 to errors.Count - 1 do begin
+    msg := errors[i];
+    ctdFriendly := ctdFriendly or 
+      ContainsText(msg, 'invalid for a light module') or 
+      ContainsText(msg, 'too many records');
+
+    AddMessage(Format(errorFmt, [msg]));
+  end;
+
+  if ctdFriendly then AddMessage(corruption);
+
+  AddMessage(sep);
 end;
 
 procedure CheckForESL(f: IInterface);
+var 
+  msg: string;
 begin
+  errors.Clear;
+
+  msg := Check(f);
+  if msg <> '' then
+    errors.Add(msg);
+
   DisplayAnalysisMessage(Name(f), CheckRecords(f));
 end;
 
@@ -80,17 +105,18 @@ var
   i: integer;
   f: IInterface;
 begin
-  HasErrors := false;
+  errors := TStringList.Create;
 
   // iterate over loaded plugins
   for i := 0 to Pred(FileCount) do begin
     f := FileByIndex(i);
     if (IsESL(f)) then CheckForESL(f);
   end;
+end;
 
-  if not HasErrors then begin
-    AddMessage('No dangerous ESL plugins were found.');
-  end;
+function Finalize: Integer;
+begin
+  errors.Free;
 end;
 
 end.
