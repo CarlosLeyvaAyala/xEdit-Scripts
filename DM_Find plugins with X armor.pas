acb9f65b4e4a6a44cd1e5c0138b29ab51fd5b656
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2023-10-10 08:55:47 -0600

    Update DM_Find plugins with X armor.pas

--- DM_Find plugins with X armor.pas
+++ DM_Find plugins with X armor.pas
@@ -6,9 +6,8 @@ uses xEditApi;
 
 implementation
 
-const
-  // armorToFind = 'Light Armor';
-  armorToFind = 'Heavy Armor';
+var
+  armorToFind: string;
 
 function IsXArmor(e: IInterface): Boolean;
 begin
@@ -31,11 +30,85 @@ begin
   end;
 end;
 
+function CreateButton(AParent: TControl): TButton;
+begin
+  Result := TButton.Create(AParent);
+  Result.Parent := AParent;
+  Result.Left := 0;
+  Result.Top := 0;
+  Result.Width := 180;
+  Result.Height := 32;
+end;
+
+function ShowForm: Integer;
+var
+  frm: TForm;  
+  btnExit, btnOk: TButton;
+  rgpOptions: TRadioGroup;
+const 
+  x = 120;
+  y = 30;
+  dy = 40;
+begin
+  frm := TForm.Create(nil);
+  Result := 0;
+
+  try
+    frm.Caption := 'Convert to armor type';
+    frm.Position := poScreenCenter;
+    frm.BorderStyle := bsDialog;
+    frm.Height := 290;
+    frm.Width := 440;
+
+    rgpOptions := TRadioGroup.Create(frm);
+    rgpOptions.Parent := frm;
+    rgpOptions.Left := x;
+    rgpOptions.Top := y; 
+    rgpOptions.Caption := 'New type';
+    rgpOptions.Items.Add('&Clothing');
+    rgpOptions.Items.Add('&Light');
+    rgpOptions.Items.Add('&Heavy');
+    rgpOptions.ItemIndex := 0;
+
+    btnExit := CreateButton(frm);
+    btnExit.Caption := '&Cancel';
+    btnExit.Cancel := true;
+    btnExit.ModalResult := mrCancel;
+    btnExit.Left := x - 85;
+    btnExit.Top := y + (4 * dy);
+
+    btnOk := CreateButton(frm);
+    btnOk.Caption := '&Ok';
+    btnOk.Default := true;
+    btnOk.ModalResult := mrOk;
+    btnOk.Left := btnExit.Left + btnExit.Width + 10;
+    btnOk.Top := btnExit.Top;
+
+    if frm.ShowModal <> mrOk then begin
+      Result := 1;
+    end
+    else begin
+      case rgpOptions.ItemIndex of
+      0: armorToFind := 'Clothing';
+      1: armorToFind := 'Light Armor';
+      2: armorToFind := 'Heavy Armor';
+      end;
+    end;
+  finally
+    frm.Release;
+  end;
+end;
+
 function Initialize: Integer;
 var 
   i: integer;
   f: IInterface;
 begin
+  Result := ShowForm;
+  if Result <> 0 then Exit;
+
+  AddMessage(#13#10#13#10);
+  
   // iterate over loaded plugins
   for i := 0 to Pred(FileCount) do begin
     f := FileByIndex(i);
@@ -43,6 +116,8 @@ begin
       AddMessage(IntToHex(GetLoadOrder(f), 2) + ' ' + GetFileName(f));
     end;
   end;
+
+  AddMessage(#13#10#13#10);
 end;
 
 end.
