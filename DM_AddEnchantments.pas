bdc40a79844f905ae812eafcf87264fb6deb2a3a
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2024-02-05 03:35:57 -0600

    Update DM_AddEnchantments.pas

--- DM_AddEnchantments.pas
+++ DM_AddEnchantments.pas
@@ -14,6 +14,9 @@ var
     gFileTo: IInterface;
     rx: TPerlRegex;
 
+const 
+    dataRx = '(.*?),(.*?),(.*?),(.*),(\d+),(\d+),(\d+)';
+
 function Initialize: Integer;
 var 
     i: integer;
@@ -123,6 +126,9 @@ begin
 
     // Edit effect
     SetElementEditValues(newEffect, 'EFID', Name(mgef));
+    SetElementEditValues(newEffect, 'EFIT\Magnitude', ValueFromLine(line, 5));
+    SetElementEditValues(newEffect, 'EFIT\Area', ValueFromLine(line, 6));
+    SetElementEditValues(newEffect, 'EFIT\Duration', ValueFromLine(line, 7));
 end;
 
 function ArmorFromLine(line: string): IInterface;
@@ -136,8 +142,6 @@ begin
 end;
 
 function ElementFromLine(line, signature: string; rxGroupEdid, rxGroupEsp: integer): IInterface;
-const 
-    dataRx = '(.*?),(.*?),(.*?),(.*)';
 begin
     Result := nil;
     rx.Subject := line;
@@ -146,23 +150,12 @@ begin
     Result := FindRecordByEdid(signature, rx.Groups[rxGroupEdid], rx.Groups[rxGroupEsp]);
 end;
 
-// Separates a string by capitals.  
-// Example:
-//  'BDO BMSNecklaceBlack' => 'BDO BMS Necklace Black' 
-function SeparateCapitals(aStr: string): string;
-var
-  r: TPerlRegex;
+function ValueFromLine(line: string; rxGroupValue: integer): IInterface;
 begin
-  r := TPerlRegex.Create;
-  try
-    r.RegEx := '((?<=[a-z])[A-Z]|[A-Z](?=[a-z]))';
-    r.Subject := aStr;
-    r.Replacement := ' \1';
-    r.ReplaceAll;
-    Result := Trim(r.Subject);
-  finally
-    r.Free;
-  end;
+    rx.Subject := line;
+    rx.RegEx := dataRx;
+    rx.Match;
+    Result := rx.Groups[rxGroupValue];
 end;
 
 function FileByName(s: string): IInterface;
@@ -197,27 +190,10 @@ begin
         AddMessage(Format('"%s" was not found in "%s".', [edid, fileName]));
 end;
 
-// function Process(e: IInterface): Integer;
-// var
-//     v: variant;
-//     s: TStringList;
-//     s, s2, basePath: string;
-//     isUnique, o, g, parentPerk: IInterface;
-//     i, j, n, m: Integer;
-//     r: Real;
-//     elem, elem2: IwbGroupRecord;
-// begin
-//     o := FindRecordByEdid('ARMO', '0AllisGoldBody', '[Melodic] All is Gold.esp');
-//     AddMessage(Name(o));  
-// end;
-
 function Finalize: Integer;
 var
   i: Integer;
 begin
-  // AddMessage(input.commaText);
-  // for i := 0 to input.Count - 1 do
-  //   AddMessage(input[i]);
   input.Free;
   rx.Free
 end;
